b6ea6bad9b9c9396352e2a12f4f77f2c7dd6d9c4
Amine Bendriouch <aminebendriouch.dev@gmail.com>
2023-04-27 09:04:31 +0100

    Fetch monsters from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,44 +1,35 @@
 import { Component } from "react";
 
-import logo from "./logo.svg";
-import "./App.css";
-
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      name: { firstName: "value", lastName: "VALUE" },
-      job: "a Developer",
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello {this.state.name.firstName} {this.state.name.lastName}. I'm{" "}
-            {this.state.job}
-          </p>
-          <button
-            onClick={() => {
-              this.setState(
-                () => {
-                  return {
-                    name: { firstName: "Amine", lastName: "BENDRIOUCH" },
-                  };
-                },
-                () => {
-                  console.log(this.state);
-                }
-              );
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+      <div>
+        {this.state.monsters.map((monster) => {
+          return <h2 key={monster.id}>{monster.name}</h2>;
+        })}
       </div>
     );
   }
